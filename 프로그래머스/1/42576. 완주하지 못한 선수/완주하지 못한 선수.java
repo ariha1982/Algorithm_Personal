bbc8d3e1ea161ec5c13470b3635ec79297316c8c
Hyeonjo Jeong <ariha1982@naver.com>
2025-06-21 14:15:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 77.99 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        
+        HashMap<String, Integer> isComplete = new HashMap<>();
+        
+        for (String person : participant) {
+            // 있으면 체이닝, 없으면 그냥 추가
+            if (isComplete.containsKey(person)) {
+                Integer personNum = isComplete.get(person);
+                isComplete.put(person, personNum + 1);
+                
+            } else {
+                isComplete.put(person, 1);
+            }
+        }
+        
+        for (String person : completion) {
+            Integer personNum = isComplete.get(person);
+            
+            if (personNum == 1) {
+                isComplete.remove(person);
+                
+            } else {
+                isComplete.put(person, personNum - 1);
+            }
+        }
+        
+        // System.out.println(new String(isComplete.keySet().stream().iterator().next()));
+        
+        String answer = new String(isComplete.keySet().stream().iterator().next());
+        return answer;
+    }
+}
